e496a90e9f2de97ede0a09e31f67f3778ebf7a34
samisamosa <samihadatta@gmail.com>
2020-04-17 21:20:15 -0400

    react compatibility

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-// console.log('starting up!');
-// const $ = require('jquery'); // es5 syntax for importing jquery
-import $ from 'jquery'; // change require to es6 import style
-import './style.scss'; // import everything to app to be used
-// $('#main').html('Here we go!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.scss';
 
-let count = 0;
-setInterval(() => {
-    $('#main').html(`You've been on this page for ${count} seconds.`);
-    // eslint-disable-next-line no-plusplus
-    count++;
-}, 1000);
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
